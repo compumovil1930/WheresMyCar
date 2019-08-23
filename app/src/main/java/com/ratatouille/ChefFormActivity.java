620dda52c707e6806a4056ca73b16c3cc0b172e5
diegogamboa21 <diegoa.gamboa@hotmail.com>
2019-08-23 17:46:47 -0500

    Actividad formulario chef e historial de servicios - Incompletas

---
+++ app/src/main/java/com/ratatouille/ChefFormActivity.java
@@ -0,0 +1,13 @@
+package com.ratatouille;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChefFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_form);
+    }
+}
---
+++ app/src/main/java/com/ratatouille/HistoryServiceActivity.java
@@ -0,0 +1,13 @@
+package com.ratatouille;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_service);
+    }
+}
