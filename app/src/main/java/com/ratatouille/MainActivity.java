c0224337ea0f81b0b67fb52e6eb8d80d30837d46
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-08-23 19:10:08 -0500

    Fix para usar actividades de google maps

--- app/src/main/java/com/ratatouille/MainActivity.java
+++ app/src/main/java/com/ratatouille/MainActivity.java
@@ -1,25 +1,35 @@
 package com.ratatouille;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView nuevaCuenta;
+    Button ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        nuevaCuenta=findViewById(R.id.textViewNueva);
+        nuevaCuenta = findViewById(R.id.textViewNueva);
         nuevaCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(v.getContext(),RegisterActivity.class);
+                Intent intent = new Intent(v.getContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        ingresar.findViewById(R.id.buttonIniciar);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), mapaDireccion.class);
                 startActivity(intent);
             }
         });
