f53f8743296cfc28981c863c37408ca0ef8e875c
gitAccCarlo <cardavid1997@gmail.com>
2019-08-26 16:06:07 -0500

    Pantalla preparando comensal

---
+++ app/src/main/java/com/ratatouille/PreparandoComensalActivity.java
@@ -0,0 +1,14 @@
+package com.ratatouille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreparandoComensalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preparando_comensal);
+    }
+}
