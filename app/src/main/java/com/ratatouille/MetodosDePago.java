cce25d98f5b431e0c68b9e9a10ec88b933d10b3f
Jairo Piza <jaipiza@hotmail.com>
2019-08-26 10:19:51 -0500

    Metodos de pago iniciado

---
+++ app/src/main/java/com/ratatouille/MetodosDePago.java
@@ -0,0 +1,14 @@
+package com.ratatouille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MetodosDePago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metodos_de_pago);
+    }
+}
