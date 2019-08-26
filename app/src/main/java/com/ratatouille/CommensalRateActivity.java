1dca01d59a64be1a3ed3b0e23aa29452f4098275
diegogamboa21 <diegoa.gamboa@hotmail.com>
2019-08-26 16:40:49 -0500

    Actividad calificación comensal

---
+++ app/src/main/java/com/ratatouille/CommensalRateActivity.java
@@ -0,0 +1,14 @@
+package com.ratatouille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommensalRateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commensal_rate);
+    }
+}
