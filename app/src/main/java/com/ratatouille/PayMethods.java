626e872e9ecffeeaee76f5234db571ebf9ed4da8
Esteban Valero <daniel-valero@javeriana.edu.co>
2019-08-27 10:52:57 -0500

    Metodos de pago finalizado

--- app/src/main/java/com/ratatouille/PayMethods.java
+++ app/src/main/java/com/ratatouille/PayMethods.java
@@ -5,22 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class PayMethods extends AppCompatActivity {
 
     ListView listView;
-
+    TextView textView;
+    String[] listItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metodos_de_pago);
-        listView = findViewById(R.id.ListView);
+
+        listView =  findViewById(R.id.listView);
+       // textView =  findViewById(R.id.textViewList);
+        listItem = getResources().getStringArray(R.array.MetodosPago);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, listItem);
+        listView.setAdapter(adapter);
+
 
     }
-    public void agregarMetodoDePago(View v){
+    public void addPayMethod(View v){
         Intent intent = new Intent(v.getContext(), AddPayMethod.class);
         startActivity(intent);
     }
