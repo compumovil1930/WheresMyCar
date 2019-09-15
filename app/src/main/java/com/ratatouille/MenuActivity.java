bc01722135f1a437d16b1abcd73b2fe56f8e9335
Diego Gustavo Guevara Bernal <diegog.guevarab@gmail.com>
2019-09-15 18:04:39 -0500

    Cambios a integración del menu

--- app/src/main/java/com/ratatouille/MenuActivity.java
+++ app/src/main/java/com/ratatouille/MenuActivity.java
@@ -3,19 +3,23 @@ package com.ratatouille;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
 
-    ListView list_menu;
-
-
+    LinearLayout list_menu;
+    TextView item_profile;
+    TextView item_payments;
+    TextView item_services;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         list_menu = findViewById(R.id.list_menu);
+        item_profile = findViewById(R.id.item_profile);
+        item_payments = findViewById(R.id.item_payments);
+        item_services = findViewById(R.id.items_services);
     }
 }
