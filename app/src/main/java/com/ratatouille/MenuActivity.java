6098d14d855487dbb4589e103dc60366ad182106
Diego Gustavo Guevara Bernal <diegog.guevarab@gmail.com>
2019-09-24 09:25:15 -0500

    intents agregados

--- app/src/main/java/com/ratatouille/MenuActivity.java
+++ app/src/main/java/com/ratatouille/MenuActivity.java
@@ -2,7 +2,9 @@ package com.ratatouille;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,9 +19,31 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        getSupportActionBar().hide();
         list_menu = findViewById(R.id.list_menu);
         item_profile = findViewById(R.id.item_profile);
         item_payments = findViewById(R.id.item_payments);
         item_services = findViewById(R.id.items_services);
+        item_services.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(v.getContext(), HistoryServiceActivity.class);
+                startActivity(intent);
+            }
+        });
+        item_payments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), PayMethods.class);
+                startActivity(intent);
+            }
+        });
+        item_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), UpdateUserProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
