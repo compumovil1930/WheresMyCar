957ff3771f389ca115f97646a3bc1b69f529a2d3
Diego Gustavo Guevara Bernal <diegog.guevarab@gmail.com>
2019-10-21 09:52:15 -0500

    Campo fecha de vencimiento corregido

--- app/src/main/java/com/ratatouille/AddPayMethod.java
+++ app/src/main/java/com/ratatouille/AddPayMethod.java
@@ -2,6 +2,9 @@ package com.ratatouille;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -19,23 +22,45 @@ public class AddPayMethod extends AppCompatActivity {
         mmaa.addTextChangedListener(new TextWatcher() {
 
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String working = s.toString();
+                boolean isValid = true;
+                mmaa.setError(null);
+                if (working.length() == 2 && before == 0) {
+                    if (Integer.parseInt(working) < 1 || Integer.parseInt(working) > 12) {
+                        isValid = false;
+                    } else {
+                        working += "/";
+                        mmaa.setText(working);
+                        mmaa.setSelection(working.length());
+                    }
+                } else if (working.length() == 5 && before == 0) {
+                    String enteredYear = working.substring(3);
+                    DateFormat df = new SimpleDateFormat("yy");
+                    String formattedDate = df.format(Calendar.getInstance().getTime());
+                    int currentYear = Integer.parseInt(formattedDate);
+                    if (Integer.parseInt(enteredYear) < currentYear) {
+                        isValid = false;
+                    }
+                } else if (working.length() != 5) {
+                    isValid = false;
+                } else
+                    isValid = true;
 
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                boolean first = true;
-                if (editable.length() == 2 && first == true) {
-                    first = false;
-                    editable.append('/');
+                if (!isValid) {
+                    mmaa.setError("Enter a valid date: MM/YY");
+                } else {
+                    mmaa.setError(null);
                 }
+            }
 
+            @Override
+            public void afterTextChanged(Editable s) {
 
             }
         });
