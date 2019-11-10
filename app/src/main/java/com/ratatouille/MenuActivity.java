3a72e8ec2ac48b0236e58efcc7fd62cbb1026038
Diego Gustavo Guevara Bernal <diegog.guevarab@gmail.com>
2019-11-09 21:40:05 -0500

    imports eliminados

--- app/src/main/java/com/ratatouille/MenuActivity.java
+++ app/src/main/java/com/ratatouille/MenuActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,7 +14,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
