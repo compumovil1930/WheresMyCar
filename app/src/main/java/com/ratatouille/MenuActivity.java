62b742fe7ee2cbc666f9bc30aa09643ae77b609a
Diego Gustavo Guevara Bernal <diegog.guevarab@gmail.com>
2019-11-10 22:02:21 -0500

    fix import

--- app/src/main/java/com/ratatouille/MenuActivity.java
+++ app/src/main/java/com/ratatouille/MenuActivity.java
@@ -15,7 +15,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
