43dbaaabaadde7293dd9a08d084df00f6b875741
Diego Gamboa <diegoa.gamboa@hotmail.com>
2019-11-20 04:51:30 -0500

    Actualizar marcador

--- app/src/main/java/com/ratatouille/ChefEnCaminoActivity.java
+++ app/src/main/java/com/ratatouille/ChefEnCaminoActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -21,6 +23,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
@@ -28,6 +31,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ratatouille.models.Chef;
+import com.ratatouille.models.Servicio;
 
 public class ChefEnCaminoActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
 
@@ -40,6 +50,12 @@ public class ChefEnCaminoActivity extends AppCompatActivity implements OnMapRead
     Button btnCancelar;
     double latitude;
     double longitude;
+
+    String keyServicio;
+    Bundle bundle;
+
+    FirebaseDatabase database;
+    DatabaseReference mDatabaseChefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +70,8 @@ public class ChefEnCaminoActivity extends AppCompatActivity implements OnMapRead
         btnCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), PreparandoComensalActivity.class);
+                Intent intent = new Intent(v.getContext(), mapaDireccion.class);
+                cambiarEstadoDeReserva();
                 startActivity(intent);
             }
         });
@@ -62,8 +79,11 @@ public class ChefEnCaminoActivity extends AppCompatActivity implements OnMapRead
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
-                            customer.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+
+                            customer.setPosition(new LatLng(latitude, longitude));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude, longitude)));
                             chef.setPosition(new LatLng(location.getLatitude()-0.05, location.getLongitude()-0.05)); // TODO: bring chef address from firebase
                             chef.setTitle("Tu chef"); //TODO: add snippet with name
                             LatLngBounds.Builder builder = new LatLngBounds.Builder();
@@ -78,6 +98,37 @@ public class ChefEnCaminoActivity extends AppCompatActivity implements OnMapRead
                 }
         );
         //TODO: move chef marker with onDataChange callback
+        //keyServicio = putExtraData("keyService");
+
+        bundle = getIntent().getBundleExtra("bundle");
+
+        mDatabaseChefs = database.getInstance().getReference("chefs");
+        mDatabaseChefs.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getChildrenCount() != 0)
+                    for (DataSnapshot singleSnap : dataSnapshot.getChildren()) {
+                        if (singleSnap != null) {
+                            Log.i("KeyChef", bundle.get("keyChef").toString());
+                            if (bundle.get("keyChef").toString().equals(singleSnap.getKey())){
+                                Chef chefEncontrado = singleSnap.getValue(Chef.class);
+
+                                //chef.remove();
+                                latitude = chefEncontrado.getDireccion().getLatitud();
+                                longitude = chefEncontrado.getDireccion().getLongitud();
+                                chef.setPosition(new LatLng(latitude, longitude));
+                                //mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude, longitude)));
+                                moveCamera(latitude, longitude, 15);
+                            }
+                        }
+                    }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
     }
 
     @Override
@@ -131,5 +182,17 @@ public class ChefEnCaminoActivity extends AppCompatActivity implements OnMapRead
             currentPolyline.remove();
         currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
         currentPolyline.setColor(0xFFF44336);
+    }
+
+    private void cambiarEstadoDeReserva(){
+        //Ingresar a firebase y cambiar el estado de la reserva a cancelado
+    }
+
+    private void moveCamera(double latitude, double longitude, int zoom){
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude))
+                .zoom(zoom)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
